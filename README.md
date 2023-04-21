73e642891b54c80072187288d347aa42be53e1f5
raphaelkw <raphael.kw@gmail.com>
2023-04-21 23:49:18 +0800

    Separated into modules

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # co-pilot-training
+
+## Running the app
+```bash
+python mini_game/main.py
+```
+
+## Running the tests
+```bash
+python -m pytest tests
+```
--- mini_game/main.py
+++ mini_game/main.py
@@ -7,14 +7,6 @@ import random
 def main():
     # define a list of choices
     choices = ["rock", "paper", "scissors", "lizard", "spock"]
-    # define a dictionary of outcomes
-    outcomes = {
-        "rock": ["scissors", "lizard"],
-        "paper": ["rock", "spock"],
-        "scissors": ["paper", "lizard"],
-        "lizard": ["paper", "spock"],
-        "spock": ["rock", "scissors"],
-    }
 
     # get the user's choice
     user_choice = input("Enter your choice: ")
@@ -22,39 +14,12 @@ def main():
     # get the computer's choice
     computer_choice = random.choice(choices)
 
-    # print the user's choice
-    print("You chose", user_choice)
-    # print the computer's choice
-    print("Computer chose", computer_choice)
-
-    # if the user's choice is in the outcomes dictionary
-    if user_choice in outcomes:
-        # if the computer's choice is in the outcomes dictionary
-        if computer_choice in outcomes:
-            # if the computer's choice is in the outcomes list
-            if computer_choice in outcomes[user_choice]:
-                # print the user wins message
-                print("You win!")
-            # if the user's choice is in the outcomes list
-            elif user_choice in outcomes[computer_choice]:
-                # print the computer wins message
-                print("Computer wins!")
-            # otherwise
-            else:
-                # print the tie message
-                print("Tie!")
-        # otherwise
-        else:
-            # print the error message
-            print("Error: Computer choice is not in outcomes dictionary")
-    # otherwise
-    else:
-        # print the error message
-        print("Error: User choice is not in outcomes dictionary")
+    # call the determine_outcome function
+    return determine_outcome(user_choice, computer_choice)
 
 
 # game starts here
-def game(user_choice, computer_choice):
+def determine_outcome(user_choice, computer_choice):
     # define a dictionary of outcomes
     outcomes = {
         "rock": ["scissors", "lizard"],
@@ -92,10 +57,12 @@ def game(user_choice, computer_choice):
         else:
             # print the error message
             print("Error: Computer choice is not in outcomes dictionary")
+            return "Error: Computer choice is not in outcomes dictionary"
     # otherwise
     else:
         # print the error message
         print("Error: User choice is not in outcomes dictionary")
+        return "Error: User choice is not in outcomes dictionary"
 
 
 # call the main function
